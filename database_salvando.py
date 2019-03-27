e56a095572ec4057b640e66f49b624c48c92cca7
nuke <rochadcmarcos@gmail.com>
2019-03-27 19:16:45 -0300

    Adicionando novos metodos e retirando o global das funçoes de database

--- database_salvando.py
+++ database_salvando.py
@@ -5,26 +5,30 @@ conn = sqlite3.connect('data.db')
 c = conn.cursor()
 data_atual = datetime.now()
 
-def insere_jogos_feitos(n_sorteio,bilhete,data=data_atual):
-	global c
-	global conn
+def insere_jogos_feitos(n_sorteio,bilhete,data=data_atual,connector=conn,cursor=c):
 	dados = [(n_sorteio,bilhete,data),]
-	c.executemany("INSERT INTO jogos_feitos VALUES (?,?,?)",dados)
+	cursor.executemany("INSERT INTO jogos_feitos VALUES (?,?,?)",dados)
 	print("Dados inseridos em jogos_feitos com sucesso!")
-	conn.commit()
+	connector.commit()
 
-def insere_jogos_ganhos(n_sorteio,bilhete,bilhete_premiado,pontos):
-	global c
-	global conn
+def insere_jogos_ganhos(n_sorteio,bilhete,bilhete_premiado,pontos,connector=conn,cursor=c):
 	dados = [(n_sorteio,bilhete,bilhete_premiado,pontos),]
-	c.executemany("INSERT INTO jogos_ganhos VALUES (?,?,?,?)",dados)
+	cursor.executemany("INSERT INTO jogos_ganhos VALUES (?,?,?,?)",dados)
 	print("Dados inseridos em jogos_ganhos com sucesso!")
-	conn.commit()
+	connector.commit()
 
-def insere_ganhadores(n_sorteio,pontos,bilhete,bilhete_premiado):
-	global c
-	global conn
-	dados = [(n_sorteio,pontos,bilhete,bilhete_premiado),]
-	c.executemany("INSERT INTO ganhadores VALUES (?,?,?,?)",dados)
-	print("Dados inseridos ganhadores com sucesso!")
-	conn.commit()
+def insere_ganhadores(n_sorteio,pontos,bilhete,bilhete_premiado,valor_premio,connector=conn,cursor=c):
+	dados = [(n_sorteio,pontos,bilhete,bilhete_premiado,valor_premio),]
+	cursor.executemany("INSERT INTO ganhadores VALUES (?,?,?,?,?)",dados)
+	print("Dados inseridos em ganhadores com sucesso!")
+	connector.commit()
+
+def insere_premio(n_sorteio,valor_total_premio,valor_20,valor_19,valor_18,valor_17,valor_16,valor_15,valor_0,connector=conn,cursor=c):
+	dados = [(n_sorteio,valor_total_premio,valor_20,valor_19,valor_18,valor_17,valor_16,valor_15,valor_0),]
+	cursor.executemany("INSERT INTO premios_valor VALUES(?,?,?,?,?,?,?,?,?)",dados)
+	print("Dados inseridos em premios com sucesso!")
+	connector.commit()
+
+
+def devolve_acumulo():
+	return aculumo
--- gerando_jogos.py
+++ gerando_jogos.py
@@ -1,6 +1,6 @@
 from gera_bilhete import confere_bilhete,gera_premio
 from datetime import datetime
-from database_salvando import insere_jogos_feitos, insere_jogos_ganhos, insere_ganhadores
+from database_salvando import insere_jogos_feitos, insere_jogos_ganhos, insere_ganhadores, insere_premio
 
 sorteio = gera_premio(20)
 print("")
@@ -10,10 +10,14 @@ seguir = input("Tecle enter para continuar:")
 arquivo_txt = ("docs/%s.txt" %(datetime.now()))
 document = open(arquivo_txt,'w')
 
-n_sorteio = 1
+n_sorteio = int(input("Digite o numero do sorteio: "))
+valor_total_premio = 0
+#valor de acumulo a ser testado
+acumulo = 1500000.00
 
-for i in range(1000000000):
+for i in range(100):
     
+    valor_total_premio += 1.50
     print("")
     jogo = ("JOGO NUMERO: %i" %i)
     print(jogo)
@@ -33,5 +37,23 @@ for i in range(1000000000):
         document.write('\nAcertos:\n')
         document.write(str(acertos_feitos))
         document.write('\n')
-
 document.close()
+
+#Calcula o valor dos premios e insere no banco de dados
+valor_devolvido = acumulo + (0.46 * valor_total_premio)
+valor_total_insere = valor_devolvido
+valor_20 = valor_devolvido * 0.28
+valor_devolvido -= valor_20
+valor_0 = valor_devolvido * 0.18
+valor_devolvido -= valor_0
+valor_19 = valor_devolvido * 0.16
+valor_devolvido -= valor_19
+valor_18 = valor_devolvido * 0.16
+valor_devolvido -= valor_18
+valor_17 = valor_devolvido * 0.7
+valor_devolvido -= valor_17
+valor_16 = valor_devolvido * 0.7
+valor_devolvido -= valor_16
+valor_15 = valor_devolvido
+#inserindo valores no banco de dados
+insere_premio(n_sorteio,valor_total_insere,valor_20,valor_19,valor_18,valor_17,valor_16,valor_15,valor_0)
